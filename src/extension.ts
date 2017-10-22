aa9c403a207f99ad0d788c167b514922e7d0a6db
junior <junior@metareal.io>
2017-10-22 12:44:46 -0400

    New stuff

--- src/extension.ts
+++ src/extension.ts
@@ -1,153 +1,306 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as commentJson from 'comment-json';
-
 import * as figlet from 'figlet';
+import * as async from 'async';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+/*
+ * Loops through the extensions to find the one containing the definition of the provided languageId.
+ * Returns the content of the corresponding "language-configuration.json" or the equivalent file.
+*/
+function getLanguageConfig(languageId:string):any {
+	var langConfig:any = null;
+	const editor:vscode.TextEditor = vscode.window.activeTextEditor;
+	const excludedLanguagesIds:any[] = ["plaintext"];
+
+	if (!excludedLanguagesIds.includes(languageId)) {
+		let langConfigFilepath:string = null;
+		for (const _ext of vscode.extensions.all) {
+			if (
+				_ext.id.startsWith("vscode.") &&
+				_ext.packageJSON.contributes &&
+				_ext.packageJSON.contributes.languages
+			) {
+				const packageLangData:any = _ext.packageJSON.contributes.languages.find(
+					_packageLangData => (_packageLangData.id === languageId)
+				);
+				if (!!packageLangData) {
+					langConfigFilepath = path.join(
+						_ext.extensionPath,
+						packageLangData.configuration
+					);
+					break;
+				}
+			}
+		}
+		if (!!langConfigFilepath && fs.existsSync(langConfigFilepath)) {
+			/**
+			 * unfortunatly, some of vscode's language config contains
+			 * comments... ("xml" and "xsl" for example)
+			 */
+			langConfig = commentJson.parse(
+				fs.readFileSync(langConfigFilepath, "utf8")
+			);
+		}
+		return langConfig;
+	}
+}
+
+/*
+ * Replaces the provided selection with a figlet banner using the provided figlet config.
+ * Formats the generated figlet banner using the provided commentTags.
+*/
+function replaceSelectionWithBanner(document, builder, selection, figletConfig, commentTags) {
+	var err = null;
+	var figletText:string = null;
+	var bannerText:string = "";
+	var useBlockComment:boolean = false;
+	var trimTrailingWhitespaces:boolean = vscode.workspace
+																							.getConfiguration("banner-comments")
+																							.get('trimTrailingWhitespaces');
+	try {
+		figletText = figlet.textSync(
+			document.getText(selection),
+			figletConfig
+		);
+		// Setting the formatting options
+		let formatAndAddLine = (_line:string, trimTrailingWhitespaces:boolean, lineComment:string = null) => {
+			if (trimTrailingWhitespaces) {
+				_line = _line.replace(/\s*$/,"");
+			}
+			if (!!lineComment) {
+				_line = lineComment + _line;
+			}
+			bannerText += _line + "\n";
+		};
+		if (!!commentTags) {
+			if (commentTags.blockComment) {
+				useBlockComment = true;
+			} else if (commentTags.lineComment) {
+				formatAndAddLine = _line => {
+					bannerText += (
+						commentTags.lineComment +
+						_line.replace(/\s*$/,"") +
+						"\n"
+					);
+				};
+			}
+		}
+		// Assemble the banner comment
+		if (useBlockComment) {
+			bannerText += commentTags.blockComment[0] + "\n";
+			figletText.split("\n").forEach(formatAndAddLine);
+			bannerText += commentTags.blockComment[1] + "\n";
+		} else {
+			figletText.split("\n").forEach(formatAndAddLine);
+		}
+	} catch (replaceErr) {
+		err = replaceErr;
+	} finally {
+		if (err) {
+			vscode.window.showErrorMessage(err.message);
+		} else {
+			builder.replace(selection, bannerText);
+		}
+	}
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let bannerCommentApply = vscode.commands.registerCommand('extension.bannerCommentApply', () => {
-		// The code you place here will be executed every time your command is executed
+	/**
+	 * Banner-comment command to apply the font to selection.
+	 */
+	let bannerCommentApply:vscode.Disposable = vscode.commands.registerCommand(
+		'extension.bannerCommentApply', () => {
+			const editor:vscode.TextEditor = vscode.window.activeTextEditor;
+			let commentTags:any = null;
 
-		const editor = vscode.window.activeTextEditor;
-
-		if (!editor) {
-			return vscode.window.showErrorMessage("No active editor (Open a file).");
-		}
-
-		let commentTags:any = null;
-		try {
-			const documentLanguageId:string = editor.document.languageId;
-			const excludedLanguagesIds:any[] = ["plaintext"];
-			if (!excludedLanguagesIds.includes(documentLanguageId)) {
-				let langConfigFilepath:string = null;
-				for (const _ext of vscode.extensions.all) {
-					if (
-						_ext.id.startsWith("vscode.") &&
-						_ext.packageJSON.contributes &&
-						_ext.packageJSON.contributes.languages
-					) {
-						const packageLangData = _ext.packageJSON.contributes.languages.find(
-							_packageLangData => (_packageLangData.id === documentLanguageId)
-						);
-						if (!!packageLangData) {
-							langConfigFilepath = path.join(
-								_ext.extensionPath,
-								packageLangData.configuration
-							);
-							break;
-						}
-					}
-				}
-				if (!!langConfigFilepath && fs.existsSync(langConfigFilepath)) {
-					/**
-					 * unfortunatly, some of vscode's language config contains
-					 * comments... ("xml" and "xsl" for example)
-					 */
-					var langConfig = commentJson.parse(
-						fs.readFileSync(langConfigFilepath, "utf8")
-					);
-					commentTags = langConfig.comments;
-				} else {
-					console.warn(
-						"Banner-comments: No matching vscode language extension found."
-					);
-				}
+			if (!editor) {
+				return vscode.window.showErrorMessage("No active editor (Open a file).");
 			}
-		} catch (error) {
-			console.error(error);
-		}
 
-		const selections:vscode.Selection[] = editor.selections;
-		const figletFont:string = vscode.workspace.getConfiguration('banner-comments').get('font');
-		const figletConfig:any = {
-			font: figletFont,
-			horizontalLayout: 'default',
-			verticalLayout: 'default'
-		};
-
-		editor.edit(builder => {
-			for (const selection of selections) {
-				var err = null;
-				var figletText:string = null;
-				var bannerText:string = "";
-				var useBlockComment:boolean = false;
-				try {
-					figletText = figlet.textSync(
-						editor.document.getText(selection),
-						figletConfig
-					);
-					// Setting the formatting steps
-					let formatAndAddLine = _line => {
-						bannerText += _line.replace(/\s*$/,"")+"\n";
-					};
-					if (!!commentTags) {
-						if (commentTags.blockComment) {
-							useBlockComment = true;
-						} else if (commentTags.lineComment) {
-							formatAndAddLine = _line => {
-								bannerText += (
-									commentTags.lineComment +
-									_line.replace(/\s*$/,"") +
-									"\n"
-								);
-							};
-						}
-					}
-					// Assemble the banner comment
-					if (useBlockComment) bannerText += commentTags.blockComment[0] + "\n";
-					figletText.split("\n").forEach(formatAndAddLine);
-					if (useBlockComment) bannerText += commentTags.blockComment[1] + "\n";
-				} catch (replaceErr) {
-					err = replaceErr;
-				} finally {
-					if (err) {
-						vscode.window.showErrorMessage(err.message);
-					} else {
-						builder.replace(selection, bannerText);
-					}
-				}
+			let langConfig:any = getLanguageConfig(editor.document.languageId);
+			if (!langConfig) {
+				console.warn(
+					"Banner-comments: No matching vscode language extension found."
+				);
+			} else {
+				commentTags = langConfig.comments;
 			}
-		});
-	});
 
-	let bannerCommentSetFont = vscode.commands.registerCommand('extension.bannerCommentSetFont', () => {
-		var opts: vscode.QuickPickOptions = {
-			matchOnDescription: true,
-			placeHolder: "What do you want to do to the selection(s)?"
-		};
-		var items: vscode.QuickPickItem[] = figlet.fontsSync().map(function (figletFont) {
-			return {
-				label: figletFont,
-				description: "Use the " + figletFont + " font",
+			const figletConfig:any = {
+				font: vscode.workspace.getConfiguration("banner-comments").get('font'),
+				horizontalLayout: 'default',
+				verticalLayout: 'default'
 			};
-		}, this);
 
-		vscode.window.showQuickPick(items).then((selectedPickerItem) => {
-			if (!selectedPickerItem) return;
-			let config = vscode.workspace.getConfiguration('banner-comments');
-			config.update('font', selectedPickerItem.label, true);
+			editor.edit(
+				(builder:vscode.TextEditorEdit) => {
+					editor.selections.forEach(
+						_selection => replaceSelectionWithBanner(
+							editor.document,
+							builder,
+							_selection,
+							figletConfig,
+							commentTags
+						)
+					);
+				}
+			);
+		}
+	);
 
-			// Debug purposes
-			console.log("Banner-comments: Updated font setting to '%s'", selectedPickerItem.label);
-		});
-	});
+	/**
+	 * Banner-comment command to select the font and save it to the workspace configuration.
+	 */
+	let bannerCommentSetFont:vscode.Disposable = vscode.commands.registerCommand(
+		'extension.bannerCommentSetFont', () => {
+			var availableFigletfonts:string[] = figlet.fontsSync();
+			var items:vscode.QuickPickItem[] = availableFigletfonts.map(
+				(figletFont:string) => {
+					return {
+						label: figletFont,
+						description: "Use the " + figletFont + " font",
+					};
+				}
+			);
+
+			vscode.window.showQuickPick(items).then(
+				(_selectedPickerItem:vscode.QuickPickItem) => {
+					if (!_selectedPickerItem) return;
+					let config:any = vscode.workspace.getConfiguration("banner-comments");
+					config.update('font', _selectedPickerItem.label, true);
+					console.log("Banner-comments: Updated font setting to '%s'", _selectedPickerItem.label);
+				}
+			);
+		}
+	);
+
+	/**
+	 * Banner-comment command to select the font from favorites and save it to the workspace configuration.
+	 */
+	let bannerCommentPickFromFavorites:vscode.Disposable = vscode.commands.registerCommand(
+		'extension.bannerCommentPickFromFavorites', () => {
+			let config:any = vscode.workspace.getConfiguration("banner-comments");
+			let favoriteFonts:string[] = config.get("favorites");
+
+			var items:vscode.QuickPickItem[] = favoriteFonts.map(
+				(_favoriteFont:string) => {
+					return {
+						label: _favoriteFont,
+						description: "Use the " + _favoriteFont + " font",
+					};
+				}
+			);
+
+			vscode.window.showQuickPick(items).then(
+				(_selectedPickerItem:vscode.QuickPickItem) => {
+					if (!_selectedPickerItem) return;
+					let fontToSetName:string = _selectedPickerItem.label;
+					config.update('font', fontToSetName, true);
+					console.log("Banner-comments: Font '%s' added to favorites.", fontToSetName);
+				}
+			);
+		}
+	);
+
+	/**
+	 * Banner-comment command add a font from the list of available figlet font to the banner-comments favorite list.
+	 */
+	let bannerCommentAddAFontToFavorites:vscode.Disposable = vscode.commands.registerCommand(
+		'extension.bannerCommentAddAFontToFavorites', () => {
+			var availableFigletfonts:string[] = figlet.fontsSync();
+			var items:vscode.QuickPickItem[] = availableFigletfonts.map(
+				(figletFont:string) => {
+					return {
+						label: figletFont,
+						description: "Use the " + figletFont + " font",
+					};
+				}
+			);
+
+			vscode.window.showQuickPick(items).then(
+				(_selectedPickerItem:vscode.QuickPickItem) => {
+					if (!_selectedPickerItem) return;
+					let config:any = vscode.workspace.getConfiguration("banner-comments");
+					let favoriteFonts:string[] = config.get("favorites");
+					let fontToAddName:string = _selectedPickerItem.label;
+					if (!favoriteFonts.includes(fontToAddName)) {
+						favoriteFonts.push(fontToAddName);
+						config.update('favorites', favoriteFonts, true);
+						console.log("Banner-comments: Font '%s' added to favorites.", fontToAddName);
+					} else {
+						vscode.window.showInformationMessage("Current font '"+fontToAddName+"' already in favorites.");
+					}
+				}
+			);
+		}
+	);
+
+	/**
+	 * Banner-comment command add the current selected font to the banner-comments favorite list.
+	 */
+	let bannerCommentAddCurrentFontToFavorites:vscode.Disposable = vscode.commands.registerCommand(
+		'extension.bannerCommentAddCurrentFontToFavorites', () => {
+			let config:any = vscode.workspace.getConfiguration("banner-comments");
+			let currentFont:string = config.get("font");
+			let favoriteFonts:string[] = config.get("favorites");
+
+			if (!favoriteFonts.includes(currentFont)) {
+				favoriteFonts.push(currentFont);
+				config.update('favorites', favoriteFonts, true);
+				console.log("Banner-comments: Font '%s' added to favorites.", currentFont);
+			} else {
+				vscode.window.showInformationMessage("Current font '"+currentFont+"' already in favorites.");
+			}
+		}
+	);
+
+	/**
+	 * Banner-comment command add the current selected font to the banner-comments favorite list.
+	 */
+	let bannerCommentRemoveFromFavorites:vscode.Disposable = vscode.commands.registerCommand(
+		'extension.bannerCommentRemoveFromFavorites', () => {
+			let config:any = vscode.workspace.getConfiguration("banner-comments");
+			let favoriteFonts:string[] = config.get("favorites");
+
+			let items:vscode.QuickPickItem[] = favoriteFonts.map(
+				(_favoriteFont:string) => {
+					return {
+						label: _favoriteFont,
+						description: "Remove the " + _favoriteFont + " font from favorites.",
+					};
+				}
+			);
+
+			vscode.window.showQuickPick(items).then(
+				(_selectedPickerItem:vscode.QuickPickItem) => {
+					if (!_selectedPickerItem) return;
+					let fontToRemoveName:string = _selectedPickerItem.label;
+					let fontToRemoveIndex:number = favoriteFonts.indexOf(fontToRemoveName);
+					if (fontToRemoveIndex >= 0) {
+						favoriteFonts.splice(fontToRemoveIndex, 1);
+						config.update('favorites', favoriteFonts, true);
+						console.log("Banner-comments: Font '%s' removed from favorites.", fontToRemoveName);
+					} else {
+						vscode.window.showInformationMessage("Font '"+fontToRemoveName+"' not in favorites.");
+					}
+				}
+			);
+		}
+	);
 
 	context.subscriptions.push(
 		bannerCommentApply,
-		bannerCommentSetFont
+		bannerCommentSetFont,
+		bannerCommentPickFromFavorites,
+		bannerCommentAddAFontToFavorites,
+		bannerCommentAddCurrentFontToFavorites,
+		bannerCommentRemoveFromFavorites
 	);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
